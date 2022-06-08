299a2be6691f20505ec9e07924c7464c1bc6f43d
Seain Malkin <dev@seain.me>
2022-06-08 22:19:45 +0800

    Initial Project Setup

---
+++ app/src/main/java/me/seain/lexim/data/AppDatabase.kt
@@ -0,0 +1,31 @@
+package me.seain.lexim.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        private var DATABASE_NAME = "Lexim_Data"
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+}
