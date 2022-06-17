d195a3b579a8b6bb7266567dfb79d4bec3ae8928
Seain Malkin <dev@seain.me>
2022-06-17 16:17:35 +0800

    LEX-8 Search feature enabled in top app bar

--- app/src/main/java/me/seain/lexim/MainActivity.kt
+++ app/src/main/java/me/seain/lexim/MainActivity.kt
@@ -1,7 +1,11 @@
 package me.seain.lexim
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
 import me.seain.lexim.databinding.ActivityMainBinding
@@ -12,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.appbar)
         setContentView(binding.root)
         supportFragmentManager.let { fm ->
             fm.commit {
@@ -22,5 +27,15 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             // TODO: Add fab action
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu?.findItem(R.id.actionSearch)?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            isIconifiedByDefault = false
+        }
+        return true
     }
 }
